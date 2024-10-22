f313668de0cb7ca86cad920fc31598956c286f1a
ecomnazar <ecomnazar.me@gmail.com>
2024-10-22 18:13:40 +0500

    [added]: token in cloud storage for testers

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,22 +2,46 @@
 
 import React from "react";
 import { redirect } from "next/navigation";
-import { getAccessTokenClient } from "@/shared/lib/cookie";
+import {
+  getAccessTokenClient,
+  setAccessTokenClient,
+} from "@/shared/lib/cookie";
 
 export default function Home() {
   React.useEffect(() => {
     const WebApp = window.Telegram.WebApp;
-
+    WebApp.BackButton.hide();
     const userLang = WebApp.initDataUnsafe.user?.language_code;
     const WebAppLanguage =
       userLang === "ru" ? "ru" : userLang === "ua" ? "ru" : "en";
 
-    WebApp.BackButton.hide();
+    // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --          -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-    const token = getAccessTokenClient();
+    const isForTesters = WebApp.initDataUnsafe?.start_param?.includes("env");
+    const accessToken = getAccessTokenClient() || "";
 
-    if (token) redirect(`${WebAppLanguage}/search`);
-    else redirect(`${WebAppLanguage}/createProfile`);
+    const tokenChecker = (token: string) => {
+      if (token) {
+        setAccessTokenClient(token);
+        redirect(`${WebAppLanguage}/search`);
+      } else redirect(`${WebAppLanguage}/createProfile`);
+    };
+
+    if (isForTesters) {
+      console.log("for test");
+      window.Telegram.WebApp.CloudStorage.getItem("token", (error, result) => {
+        if (error) {
+          console.log("err, ", error);
+          tokenChecker("");
+        } else {
+          console.log("result, ", result);
+          tokenChecker(result || "");
+        }
+      });
+    } else {
+      console.log("for prod");
+      tokenChecker(accessToken);
+    }
   }, []);
 
   return null;
