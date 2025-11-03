722ea5ec0fcf87ed4a0b0766c0e0fb796652ebf5
Nikita Kuzmin Vladmirovich <kuzya@MacBook-Pro-2.local>
2025-11-03 11:54:02 +0300

    bruh

--- src/app/[locale]/(bottomNavbar)/profile/ui/WalletNotConnectedCard.tsx
+++ src/app/[locale]/(bottomNavbar)/profile/ui/WalletNotConnectedCard.tsx
@@ -3,32 +3,18 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-import { useConnectTonWallet } from "@/shared/hooks/useConnectTonWallet";
 import { Button } from "@/shared/ui/Button";
 import { Card } from "@/shared/ui/Card";
 import { Step } from "@/shared/ui/Step";
 
 export const WalletNotConnectedCard = () => {
   const { t } = useTranslation();
-  const connectTonWallet = useConnectTonWallet();
 
   const onWalletConnect = () => connectTonWallet();
   // toggleModal("payment-network-types", null, true);
 
   return (
     <Card>
-      <Step
-        title={t("connectTonWallet")}
-        subTitle={t("connectTonWalletToGetBonuses")}
-        icon="ton"
-        actions={
-          <Button
-            onClick={onWalletConnect}
-            text={t("connectWallet")}
-            className="mt-3 !w-fit px-4 bg-gradient-to-b to-gradientPrimary from-gradientSecondary"
-          />
-        }
-      />
       <Step
         title={t("getBonuses")}
         subTitle={t("getBonusesDaily")}
